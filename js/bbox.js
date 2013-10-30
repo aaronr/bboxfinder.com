79864f5b28806d9fb4cbcbde1c88b195e669b114
Aaron Racicot <aaronr@z-pulley.com>
2013-10-29 21:51:45 -0700

    Basic mouse move and zoom level display

--- js/bbox.js
+++ js/bbox.js
@@ -3,5 +3,14 @@ var map = null;
 $(function() {
     map = L.mapbox.map('map', 'examples.map-9ijuk24y')
         .setView([48, -122], 5);
+    $('#zoomlevel').val(map.getZoom().toString());
+
+    map.on('mousemove', function(e) {
+        $('#mousepos').val(e.latlng.toString());
+    });
+    map.on('zoomend', function(e) {
+        $('#zoomlevel').val(map.getZoom().toString());
+    });
+
 });
 
