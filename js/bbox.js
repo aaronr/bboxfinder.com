0c382681876f0f6210eb916181f2b7ae6f6ac383
Aaron Racicot <aaronr@z-pulley.com>
2013-10-29 22:02:53 -0700

    Basic bounds

--- js/bbox.js
+++ js/bbox.js
@@ -4,9 +4,11 @@ $(function() {
     map = L.mapbox.map('map', 'examples.map-9ijuk24y')
         .setView([48, -122], 5);
     $('#zoomlevel').val(map.getZoom().toString());
+    $('#mapbounds').val(map.getBounds().toBBoxString());
 
     map.on('mousemove', function(e) {
         $('#mousepos').val(e.latlng.toString());
+        $('#mapbounds').val(map.getBounds().toBBoxString());
     });
     map.on('zoomend', function(e) {
         $('#zoomlevel').val(map.getZoom().toString());
