303513cd92f7dddd50e8e8e31c72f6700db3455a
Aaron Racicot <aaronr@z-pulley.com>
2013-10-30 09:22:49 -0700

    Add in basic drawing tools

--- js/bbox.js
+++ js/bbox.js
@@ -3,6 +3,23 @@ var map = null;
 $(function() {
     map = L.mapbox.map('map', 'examples.map-9ijuk24y')
         .setView([48, -122], 5);
+
+    // Initialize the FeatureGroup to store editable layers
+    var drawnItems = new L.FeatureGroup();
+    map.addLayer(drawnItems);
+    
+    // Initialize the draw control and pass it the FeatureGroup of editable layers
+    var drawControl = new L.Control.Draw({
+        edit: {
+            featureGroup: drawnItems
+        }
+    });
+    map.addControl(drawControl);
+
+    map.on('draw:created', function (e) {
+        drawnItems.addLayer(e.layer);
+    });
+    
     $('#zoomlevel').val(map.getZoom().toString());
     $('#mapbounds').val(map.getBounds().toBBoxString());
 
