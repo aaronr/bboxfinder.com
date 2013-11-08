cc690fccbd3caf8fba82f8c97e61f97dbbdf2844
thebigspoon <gregcorradini@gmail.com>
2013-11-07 22:33:10 -0800

    create LightBox singleton that doesn't resize every time it appears

--- js/bbox.js
+++ js/bbox.js
@@ -2,6 +2,73 @@ var map = null;
 
 /*
 **
+**  create a lightBox instance
+**  really b/c we need a singleton
+**
+*/
+var lightBox = (function(){
+    "use strict";
+
+    var LightBox = function(options){
+
+        options || (options={});
+    
+        this.cWidth = null;
+        this.cHeight = null;
+
+    };
+    
+
+    LightBox.prototype.startLightBox = function() {
+
+        $("#ocontainer")
+            .before('<div class="overlay"></div>')
+
+        $(".overlay")
+            .animate({"opacity":"0.6"}, 200, "linear");
+
+        $(".ocontainer")
+            .css( "display", "inherit" );
+
+        // use width and height to dynamically position
+        if( !(this.cWidth) && !(this.cHeight) ){
+            this.cWidth = $(".ocontainer textarea").width();
+            this.cHeight = $(".ocontainer textarea").height();
+        }
+
+        $(".ocontainer")
+            .css({
+                "top":        "50%",
+                "left":        "50%",
+                "width":      this.cWidth + 20,
+                "height":     this.cHeight + 20,
+                "margin-top": -(this.cHeight/2), // the middle position
+                "margin-left":-(this.cWidth/2)
+            })
+            .animate({"opacity":"1"}, 200, "linear");
+
+        
+        $("#map .leaflet-tile-loaded").addClass( "blurred" );
+    };
+
+    LightBox.prototype.endLightBox = function(){
+
+        $('.ocontainer').css("display", "none" );
+        $('#map .leaflet-tile-loaded').removeClass('blurred');
+        $('#map .leaflet-tile-loaded').addClass('unblurred');
+        setTimeout( function(){
+            $('#map .leaflet-tile-loaded').removeClass('unblurred');
+        },7000);
+        $('.overlay').remove();
+
+    };
+
+    return new LightBox();
+
+})(); // create it right away
+
+/*
+**
 **  override L.Rectangle 
 **  to fire an event after setting
 **
@@ -18,46 +85,6 @@ L.Rectangle.prototype.setBounds = function (latLngBounds) {
     this.fire( 'bounds-set' );
 }
 
-function startLightBox() {
-
-    $("#ocontainer")
-        .before('<div class="overlay"></div>')
-
-    $(".overlay")
-        .animate({"opacity":"0.6"}, 200, "linear");
-
-    $(".ocontainer").css( "display", "inherit" );
-
-    // use width and height to dynamically position
-    var cWidth = $(".ocontainer textarea").width();
-    var cHeight = $(".ocontainer textarea").height();
-
-    $(".ocontainer")
-        .css({
-            "top":        "50%",
-            "left":        "50%",
-            "width":      cWidth + 20,
-            "height":     cHeight + 20,
-            "margin-top": -(cHeight/2), // the middle position
-            "margin-left":-(cWidth/2)
-        })
-        .animate({"opacity":"1"}, 200, "linear");
-
-    
-    $("#map .leaflet-tile-loaded").addClass( "blurred" );
-}
-
-function endLightBox(){
-
-    $('.ocontainer').css("display", "none" );
-    $('#map .leaflet-tile-loaded').removeClass('blurred');
-    $('#map .leaflet-tile-loaded').addClass('unblurred');
-    setTimeout( function(){
-        $('#map .leaflet-tile-loaded').removeClass('unblurred');
-    },7000);
-    $('.overlay').remove();
-
-}
 
 function addLayer(layer, name, zIndex, on) {
     if (on) {
@@ -405,13 +432,13 @@ $(function() {
     });
 
     // handle create-geojson click events
-    $('#create-geojson').on( 'click' , startLightBox );
+    $('#create-geojson').on( 'click' , lightBox.startLightBox );
 
-    $('button#cancel').on( 'click', endLightBox );
+    $('button#cancel').on( 'click', lightBox.endLightBox );
 
     $('button#add').on( 'click', function(evt){
         var is_valid = addGeoJson();
-        endLightBox();
+        if( is_valid ) endLightBox();
     });
 
     // Add in a layer to overlay the tile bounds of the google grid
